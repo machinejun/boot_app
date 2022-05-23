90b7db7285252f31cd6e2b281b25f3ce9e10862a
machinejun <akswns03@naver.com>
2022-05-23 22:56:47 +0900

    BMIcalculator

--- bmiapp/app/src/main/java/com/example/bmiapp/Activity2.java
+++ bmiapp/app/src/main/java/com/example/bmiapp/Activity2.java
@@ -3,29 +3,50 @@ package com.example.bmiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Activity2 extends AppCompatActivity {
+    private LinearLayout underweight;
+    private LinearLayout normal;
+    private LinearLayout overweight;
+    private LinearLayout obesity;
+    private LinearLayout extermelyfat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
+        underweight = findViewById(R.id.underweight);
+        normal = findViewById(R.id.normalfat);
+        overweight = findViewById(R.id.overweight);
+        obesity = findViewById(R.id.obesity);
+        extermelyfat = findViewById(R.id.external);
+
+        underweight.setVisibility(View.INVISIBLE);
+        normal.setVisibility(View.INVISIBLE);
+        overweight.setVisibility(View.INVISIBLE);
+        obesity.setVisibility(View.INVISIBLE);
+        extermelyfat.setVisibility(View.INVISIBLE);
+
+
         if(getIntent() != null){
             int result = calculateBMI(getIntent().getDoubleArrayExtra("info"));
             Log.d("TAG","결과: " + result);
 
             if(result < 18){
-
+                underweight.setVisibility(View.VISIBLE);
             }else if(result < 23){
-
+                normal.setVisibility(View.VISIBLE);
             }else if(result < 25){
-
+                overweight.setVisibility(View.VISIBLE);
             }else if(result < 30){
-
+                obesity.setVisibility(View.VISIBLE);
             }else{
-
+                extermelyfat.setVisibility(View.VISIBLE);
             }
         }
     }
