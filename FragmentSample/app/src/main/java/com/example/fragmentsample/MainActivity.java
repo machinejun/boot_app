4e453bb9819b750cb0ffacacd62e74772d60a77f
machinejun <akswns03@naver.com>
2022-05-30 21:06:20 +0900

    homeWork

---
+++ FragmentSample/app/src/main/java/com/example/fragmentsample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.fragmentsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.ListFragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
